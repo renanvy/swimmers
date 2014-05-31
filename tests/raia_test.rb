3c3ff2f839c513094013e73803f7ec0226780f66
Renan <renanvy@gmail.com>
2014-05-31 17:13:34 -0300

    Started method define_raia

---
+++ tests/raia_test.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+require '../lib/swimmer'
+require '../lib/raia'
+
+class RaiaTest < Test::Unit::TestCase
+
+  def teste_deve_adicionar_o_nadador_com_melhor_tempo_na_raia_4
+    swimmers = []
+    swimmers << Swimmer.new(name: "Renan", birth_year: "1991", time: "1,8")
+    swimmers << Swimmer.new(name: "Matheus", birth_year: "1991", time: "1,7")
+
+    raia = Raia.new(swimmers)
+    assert_equal(raia.define_raia!, "Matheus") 
+  end
+end
